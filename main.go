6b06f2ab8e404c089af6674782d5492a13eaf1ac
Giuliano <giuliano.de.paolis@accenture.com>
2019-09-05 17:27:59 +0900

    Update main.go

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	title := "Jenkins X golang http example"
+	title := "Jenkins X golang http example V.0.0.2"
 
 	from := ""
 	if r.URL != nil {
